be73973bc9945aca2d9ac9f28eeacea55cf01282
Arthur Gabriel <arthurgdasilva77@gmail.com>
2024-05-28 19:42:10 -0300

    a

--- src/components/BoxContent.tsx
+++ src/components/BoxContent.tsx
@@ -6,7 +6,7 @@ type Props = {
 export const BoxContent = ({ title, number }: Props) => {
     return(
         <div 
-        className="bg-[#825734] text-white border-black border w-36 h-36 flex flex-col items-center justify-center text-center gap-4 px-2 rounded-md 2xl:w-56 2xl:h-56 min-[2000px]:w-72 min-[2000px]:h-72px min-[2000px]:text-4xl"
+        className="bg-[#825734] text-white border-black border w-24 h-24 flex flex-col items-center justify-center text-center gap-4 px-2 rounded-md md:w-36 md:h-36 2xl:w-56 2xl:h-56 min-[2000px]:w-72 min-[2000px]:h-72px min-[2000px]:text-4xl"
         >
             <h3>{title}</h3>
             <h4>{number}</h4>
